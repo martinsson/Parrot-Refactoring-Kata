5c9fcd1ecaeac8a7789946e6af65085400215ba0
johan.martinsson <johan.martinsson@dexyos.fr>
2022-05-11 18:29:36 +0200

    finished kata

---
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,19 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+    private int numberOfCoconuts;
+
+    public AfricanParrot(int numberOfCoconuts) {
+        this.numberOfCoconuts = numberOfCoconuts;
+    }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
+    private double getLoadFactor() {
+        return 9.0;
+    }
+
+}
--- Java/src/main/java/parrot/ParrotTypeEnum.java
+++ Java/src/main/java/parrot/ParrotTypeEnum.java
@@ -5,4 +5,5 @@ public enum ParrotTypeEnum {
     EUROPEAN,
     AFRICAN,
     NORWEGIAN_BLUE
+
 }
